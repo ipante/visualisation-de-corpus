adebedcdfe612b0ed86b947b9d3967133ddda4ce
Isaac Pante <isaac.pante@unil.ch>
2019-07-22 14:53:33 +0200

    Ajout d'un fichier index_10.js

--- js/index_10.js
+++ js/index_10.js
@@ -1,1 +1,39 @@
-/* VIDE */
+/* CREATION D'UNE BARRE PAR % DE SCHWA PAR LOCUTEUR */
+
+d3.csv("donnees.csv", function(d){
+    return {
+        match : +d.Match[0],
+        speaker : d.Speaker,
+        survey : d.Survey,
+        task : d.Task           
+      }    
+}).then(function(data) {
+    let donnees_groupees = d3.nest()
+        .key(function(d) { return d.survey; })
+        .rollup(function(v) {
+            return +(100 * d3.mean(v, d => d.match)).toFixed(2)
+        })
+        .entries(data);
+        console.log(Données_groupees);
+
+    let canevas = d3.select('body').append('svg');
+        canevas.append('rect')
+            .attr('x', 10)
+            .attr('y', 5)
+            .attr('width', 1000)
+            .attr('height', 2)
+
+        // ajout des rectangles
+        canevas.selectAll('rect')
+            // joindre les données
+            .data(donnees_groupees)
+            // gestion des nouveaux noeuds
+            .enter()
+            .append('rect')
+                .attr('x', 10)
+                .attr('y', (d,i) => i*10)
+                .attr('width', d => (d.value*1000)/100)
+                .attr('height', 8)
+    
+    console.log(donnees_groupees);
+});
